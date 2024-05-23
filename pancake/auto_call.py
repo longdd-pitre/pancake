ec9dc7add1343532dcf4c64501e939dfe7460f13
longdd-pitre <125118290+longdd-pitre@users.noreply.github.com>
2024-05-23 14:56:34 +0700

    Create auto_call.py

---
+++ pancake/auto_call.py
@@ -0,0 +1,25 @@
+import phonenumbers
+import subprocess
+
+def main():
+    # Đọc file danh sách số điện thoại
+    with open('danh_sach_dien_thoai.txt', 'r') as f:
+        danh_sach_so = f.readlines()
+
+    # Khởi tạo Phone Link
+    subprocess.Popen(['start', 'ms-phone-link:', ''], shell=True)
+
+    # Duyệt qua danh sách số và gọi điện
+    for so_dien_thoai in danh_sach_so:
+        # Xử lý số điện thoại
+        so_chuan = phonenumbers.parse(so_dien_thoai.strip(), 'VN')
+        so_dien_thoai_chuan = phonenumbers.format_number(so_chuan, phonenumbers.PhoneNumberFormat.E164)
+
+        # Gọi điện thoại
+        subprocess.Popen(['ms-phone-link', 'dial', so_dien_thoai_chuan], shell=True)
+
+       
+        time.sleep(5)
+
+if __name__ == '__main__':
+    main()
